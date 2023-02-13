a165530e856e0b7da0ac48d7525e232ad1fda3bd
Karan Gupta <d22124440@mytudublin.ie>
2023-02-13 22:25:12 +0000

    events

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -4,9 +4,23 @@ const Content = () => {
     const int = Math.floor(Math.random() * names.length);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log("You clicked");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click Here</button>
+      <button onClick={() => handleClick2("Karan")}>Click Here</button>
+      <button onClick={(e) => handleClick3(e)}>Click Here Next</button>
     </main>
   );
 };
